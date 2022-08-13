c3e074ecdec79a386adf5d78a396657477bcd174
Guy Marshall <guymarshall9@gmail.com>
2022-08-13 09:10:25 +0100

    Remove unnecessary comments

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-# prompt user for filename
 filename = input("Enter filename: ")
 
-# prompt user for search_string
 search_string = input("Enter string to search for: ")
-# convert to open with syntax
+
 with open(filename, "r") as file:
     for line in file:
         if search_string in line:
