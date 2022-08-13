37c07f105a3f7a73fadc884e207b2a28ac31c1b7
Guy Marshall <guymarshall9@gmail.com>
2022-08-13 09:08:44 +0100

    Add prompts for filename and search_string

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # prompt user for filename
+filename = input("Enter filename: ")
 
 # prompt user for search_string
+search_string = input("Enter string to search for: ")
 # convert to open with syntax
 
 file = open("sample.txt", "r")
