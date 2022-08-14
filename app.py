d598303a7c2f1d0c688e26a98021bee65085480c
Guy Marshall <guymarshall9@gmail.com>
2022-08-14 19:49:33 +0100

    Add results to array and save to file

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 filename = input("Enter filename: ")
 
 search_string = input("Enter string to search for: ")
+output = []
 
 with open(filename, "r") as file:
     for line in file:
         if search_string in line:
-            print(line)
+            output.append(line)
+            # print(line)
+
+with open(f"{search_string}.txt", "w") as file:
+    for line in output:
+        file.write(line)
